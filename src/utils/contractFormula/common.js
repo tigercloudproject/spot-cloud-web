5c46122711a5c011822d1c6ca29ed57eafce5162
张冲 <675425730@qq.com>
2019-10-15 20:21:39 +0800

    删除src下不用的文件

--- src/utils/contractFormula/common.js
+++
@@ -1,8 +0,0 @@
-// IsReverse 是否是反向合约
-export function IsReverse(QuoteCoin, PriceCoin) {
-    return QuoteCoin === PriceCoin
-}
-// MarginCoin 保证金币
-export function MarginCoin(BaseCoin, QuoteCoin, PriceCoin) {
-    return IsReverse(QuoteCoin, PriceCoin) ? BaseCoin : QuoteCoin
-}
